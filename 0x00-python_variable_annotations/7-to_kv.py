e7f1737f011eec467297b3ef94f6d10d0a328e7b
ghbouzrbay <atighmarcelline@gmail.com>
2024-01-11 19:31:08 +0100

    mondatory

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''type-annotated function to_kv that takes a string k
+    and an int OR float v as arguments and returns a tuple
+'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
